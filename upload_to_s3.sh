42ac0cafcd2aa9aedaaa680c388147232e102569
Mark Doten <mark.doten@gmail.com>
2017-08-03 11:16:32 -0700

    Remove s3cmd recommended content type and let s3 decide (#4)

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -155,5 +155,5 @@ else
     fi
 fi
 
-s3cmd sync -M --acl-public --add-header $MAXAGE_HEADER $ENCODING_HEADER "$TEMPDIR/" "$BUCKET_URL"
+s3cmd sync --acl-public --add-header $MAXAGE_HEADER $ENCODING_HEADER "$TEMPDIR/" "$BUCKET_URL"
 rm -rf $TEMPDIR
